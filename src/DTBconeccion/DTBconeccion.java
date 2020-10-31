83ce5af7ab003777abe43b898fc901c00c659cb2
Edison Padilla <edisonjpp@MacBook-Pro-de-Edison.local>
2020-10-31 19:23:51 -0400

    PROJECT-0 #coneccion a la base de datos creada y libreria de RSMaterialComponets implementada

---
+++ src/DTBconeccion/DTBconeccion.java
@@ -0,0 +1,48 @@
+package DTBconeccion;
+
+import com.mysql.jdbc.Connection;
+
+import javax.swing.*;
+import java.sql.DriverManager;
+
+public class DTBconeccion {
+
+    private  static Connection connect ;
+    private static String drive = "com.mysql.jdbc.Driver";
+    private static String user = "root";
+    private static String password = "";
+    private static String url = "jdbc:mysql://localhost:3306/tarea6";
+
+    public  void connectar(){
+        connect = null ;
+        try{
+            Class.forName(drive);
+            connect = (Connection) DriverManager.getConnection(url ,user , password);
+
+            if(connect != null)
+                System.out.println("Base de datos iniciada!");
+            else
+                alert("Base de datos caida");
+
+        }catch (Exception e ){
+           System.out.println(e);
+        }
+    }
+
+    public Connection getConeccion(){
+        return  connect;
+    }
+
+    public void cortarConeccion(){
+        connect = null;
+        if(connect == null)
+            alert("Base de datos desconectada");
+        else
+            alert("No se desconecto.");
+    }
+
+    void alert(String message){
+        JOptionPane.showMessageDialog(null , message);
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import DTBconeccion.DTBconeccion;
+
 public class Main {
     public  static void main(String[] args){
         System.out.println("Keloke bro' ,  mete mano!");
+        DTBconeccion coneccion = new DTBconeccion();
+        coneccion.connectar();
+
+        // ese es el get de la coneccion , asi accedes a ella.
+        // coneccion.getConeccion();
     }
 }
